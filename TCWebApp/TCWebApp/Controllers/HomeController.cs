11be151ecab1886b9f2791b8ca15d3aef86a4aa7
DESKTOP10\lucky <baljeetkalsi2016@gmail.com>
2022-07-22 19:36:00 -0400

    initial commit
    initial commit

---
+++ TCWebApp/TCWebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TCWebApp.Models;
+
+namespace TCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {        
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserData User)
+        {
+            Session["UserName"] = User.UserName.ToString();
+
+            return RedirectToAction("Flyer");
+        }
+
+        public ActionResult Flyer()
+        {
+            return View();
+        }
+    }
+}
